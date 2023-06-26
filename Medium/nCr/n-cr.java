07e829aaeb8f3541fb46b5474727a07f1631dd35
Ankit Kumar <89263177+akb6406@users.noreply.github.com>
2023-06-26 23:28:45 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/nCr/n-cr.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String read[] = in.readLine().trim().split("\\s+");
+            int n = Integer.parseInt(read[0]);
+            int r = Integer.parseInt(read[1]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.nCr(n, r));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution{
+    static int nCr(int n, int r)
+    {
+        // code here
+           if(n<r) return 0;
+        if(r==n || r==0) return 1;
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        int mod = 1000000007;
+        for (int i=1; i<=n; i++) {
+            for (int j = Math.min(i,r); j>0; j--) {
+                dp[j] = (dp[j] + dp[j-1]) % mod;
+            }
+        }
+        return dp[r];
+    }
+}
