74668f5b3476ad2641ee5582a0024401acebe678
Ankit Kumar <89263177+akb6406@users.noreply.github.com>
2023-07-08 11:45:38 +0530

    Time: 37 ms (97.73%), Space: 59.8 MB (21.59%) - LeetHub

--- 2681-put-marbles-in-bags/2681-put-marbles-in-bags.java
+++ 2681-put-marbles-in-bags/2681-put-marbles-in-bags.java
@@ -1,29 +1,29 @@
 class Solution {
     public long putMarbles(int[] weights, int k) {
         int n= weights.length;
-        // int sum[]= new int[n];
-        // for(int i=0;i<n-1;i++){
-        //     sum[i]= weights[i]+weights[i+1];
-        // }
-        // Arrays.sort(sum);
-        // long minsum=0;
-        // long maxsum=0;
-        // for(int i=0;i<k-1;i++){
-        //     minsum+=weights[i];
-        //     maxsum+=weights[n-2-i];
-        // }
-        // return maxsum-minsum;
-        List<Integer> candidates = new ArrayList<>();
-        for (int i = 0; i < n - 1; i++) {
-            candidates.add(weights[i] + weights[i+1]);
+        int sum[]= new int[n-1];
+        for(int i=0;i<n-1;i++){
+            sum[i]= weights[i]+weights[i+1];
         }
-        Collections.sort(candidates);
-        long min_sum = 0, max_sum = 0;
-        for (int i = 0; i < k - 1; i++) {
-            min_sum += candidates.get(i);
-            max_sum += candidates.get(n - 2 - i);
+        Arrays.sort(sum);
+        long minsum=0;
+        long maxsum=0;
+        for(int i=0;i<k-1;i++){
+            minsum+=sum[i];
+            maxsum+=sum[n-2-i];
         }
-        return max_sum - min_sum;
+        return maxsum-minsum;
+        // List<Integer> candidates = new ArrayList<>();
+        // for (int i = 0; i < n - 1; i++) {
+        //     candidates.add(weights[i] + weights[i+1]);
+        // }
+        // Collections.sort(candidates);
+        // long min_sum = 0, max_sum = 0;
+        // for (int i = 0; i < k - 1; i++) {
+        //     min_sum += candidates.get(i);
+        //     max_sum += candidates.get(n - 2 - i);
+        // }
+        // return max_sum - min_sum;
 
     }
 }
