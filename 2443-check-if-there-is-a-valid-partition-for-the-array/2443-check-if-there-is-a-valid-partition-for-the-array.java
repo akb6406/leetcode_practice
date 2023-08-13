b031803cfa07d8cbf65b3ff80e939eb8744fb812
Ankit Kumar <89263177+akb6406@users.noreply.github.com>
2023-08-13 15:56:02 +0530

    Time: 9 ms (68.93%), Space: 56.9 MB (51.46%) - LeetHub

---
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/2443-check-if-there-is-a-valid-partition-for-the-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean validPartition(int[] nums) {
+        int n= nums.length;
+        boolean dp[]= new boolean[n+1];
+       dp[0]=true;
+        return f(nums,n,dp);
+    }
+    public static boolean f(int nums[], int n, boolean dp[]){
+        for(int i=0;i<n;i++){
+            int ind= i+1;
+            if(i>0 && nums[i]==nums[i-1]){
+                dp[ind]= dp[ind] | dp[ind-2];
+            }
+            if(i>1 && nums[i]==nums[i-1] && nums[i]==nums[i-2]){
+                dp[ind] = dp[ind-2] | dp[ind-3];
+            }
+            if(i>1 && nums[i]==nums[i-1]+1 && nums[i]==nums[i-2]+2){
+                dp[ind]= dp[ind] | dp[ind-3];
+            }
+        }
+        return dp[n];
+
+
+    }
+}
