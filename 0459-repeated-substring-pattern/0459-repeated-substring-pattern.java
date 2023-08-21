9c6e899f5d22e7a3d7c9bd40d5a636ff946986e1
Ankit Kumar <89263177+akb6406@users.noreply.github.com>
2023-08-21 08:32:58 +0530

    Time: 62 ms (57.04%), Space: 44.3 MB (32.14%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int n= s.length();
+        for(int i=1;i<=n/2;i++){
+            if(n%i==0){
+                StringBuilder sb= new StringBuilder();
+                for(int j=0;j<n/i ; j++){
+                    sb.append(s.substring(0,i));
+                }
+                if(s.equals(sb.toString())){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
